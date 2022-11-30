7619a7ecd594bac9f2856ee77375746d512b5797
Josue Vargas <josuevargass@hotmail.com>
2022-11-29 19:48:16 -0500

    udapte: knowledge 2

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
-export class LandingPageComponent {
+export class LandingPageComponent implements OnInit {
+
+  knowledges:any = []
+
+  constructor() {}
+
+  ngOnInit() {
+    this.knowledges = [
+      {
+        logo: '../../../assets/img/angularLogo.png',
+        name: 'Angular',
+      },
+
+      {
+        logo: '../../../assets/img/typescriptLogo.png',
+        name: 'Typescript',
+      },
+      {
+        logo: '../../../assets/img/javascriptLogo.png',
+        name: 'Javascript',
+      },
+      {
+        logo: '../../../assets/img/ionicLogo.png',
+        name: 'Ionic',
+      }
+    ]
+  }
 
 }
