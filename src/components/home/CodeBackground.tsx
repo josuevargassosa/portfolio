006315923ea647d12e0207a7ae4a88d9afc0a031
Josue Vargas <josuevargass@hotmail.com>
2026-03-20 22:53:24 -0500

    fix: increase gradient mesh opacity to be clearly visible

--- src/components/home/CodeBackground.tsx
+++ src/components/home/CodeBackground.tsx
@@ -7,10 +7,10 @@ export function CodeBackground() {
       aria-hidden="true"
     >
       {/* Gradient orbs */}
-      <div className="absolute -top-[40%] -left-[20%] w-[60%] h-[60%] rounded-full bg-purple-500/[0.04] dark:bg-purple-500/[0.07] blur-[120px] animate-mesh-1" />
-      <div className="absolute -top-[20%] -right-[20%] w-[50%] h-[50%] rounded-full bg-blue-500/[0.04] dark:bg-blue-500/[0.07] blur-[120px] animate-mesh-2" />
-      <div className="absolute top-[40%] -left-[10%] w-[40%] h-[40%] rounded-full bg-cyan-500/[0.03] dark:bg-cyan-500/[0.05] blur-[100px] animate-mesh-3" />
-      <div className="absolute bottom-[10%] right-[10%] w-[35%] h-[35%] rounded-full bg-indigo-500/[0.03] dark:bg-indigo-500/[0.06] blur-[100px] animate-mesh-4" />
+      <div className="absolute -top-[30%] -left-[10%] w-[60%] h-[60%] rounded-full bg-purple-500/15 dark:bg-purple-500/20 blur-[120px] animate-mesh-1" />
+      <div className="absolute -top-[10%] -right-[10%] w-[50%] h-[50%] rounded-full bg-blue-500/10 dark:bg-blue-500/15 blur-[120px] animate-mesh-2" />
+      <div className="absolute top-[50%] -left-[5%] w-[40%] h-[40%] rounded-full bg-cyan-500/10 dark:bg-cyan-500/12 blur-[100px] animate-mesh-3" />
+      <div className="absolute bottom-[5%] right-[5%] w-[35%] h-[35%] rounded-full bg-indigo-500/10 dark:bg-indigo-500/15 blur-[100px] animate-mesh-4" />
     </div>
   );
 }
